735450ce304fc93cc5bc755e5d1a3ece971e2b1b
Kowshique Roy <kowshiqueroy@gmail.com>
2022-10-10 00:24:45 +0600

    2

--- APPFreeLance/app/src/main/java/com/example/thaibank/MainActivity.java
+++ APPFreeLance/app/src/main/java/com/example/thaibank/MainActivity.java
@@ -1,12 +1,14 @@
-package com.example.myapplication;
+package com.example.thaibank;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppComzatActivity {
+public class MainActivity extends AppCompatActivity {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 }
